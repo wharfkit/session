f42d8ed2c230ed344b81590e6a087f41038672a7
Aaron Cox <aaron@greymass.com>
2023-02-24 21:11:01 -0500

    Fixed mock context

--- test/utils/mock-context.ts
+++ test/utils/mock-context.ts
@@ -1,17 +1,31 @@
-import {ABICache, ChainDefinition, TransactContext, TransactContextOptions} from '$lib'
+import {
+    ABICache,
+    ChainDefinition,
+    Session,
+    SigningRequest,
+    TransactArgs,
+    TransactContext,
+    TransactContextOptions,
+} from '$lib'
 import {APIClient, FetchProvider, PermissionLevel} from '@greymass/eosio'
 
 import {mockChainDefinition, mockUrl} from './mock-config'
 import {mockFetch} from '$test/utils/mock-fetch'
+import {mockSession, mockSessionArgs, mockSessionOptions} from './mock-session'
 
 const client = new APIClient({
     provider: new FetchProvider(mockUrl, {fetch: mockFetch}),
 })
 
+const session = new Session(mockSessionArgs, mockSessionOptions)
+const abiProvider = new ABICache(client)
+
 export const mockTransactContextOptions: TransactContextOptions = {
-    abiProvider: new ABICache(client),
+    abiProvider,
     chain: ChainDefinition.from(mockChainDefinition),
     client,
+    createRequest: async (args: TransactArgs): Promise<SigningRequest> =>
+        session.createRequest(args, abiProvider),
     fetch: mockFetch,
     permissionLevel: PermissionLevel.from('wharfkit1125@test'),
 }
