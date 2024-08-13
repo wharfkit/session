2274ace697107acd59ae4c3d4360b2f2b58c90b9
Aaron Cox <aaron@greymass.com>
2024-08-13 11:12:30 -0700

    Disabling resource provider check
    Disabling because the reproducibility of this is difficult due to the varying responses from the server. We should be doing this in the resource provider plugin anyways.

--- test/tests/plugins.ts
+++ test/tests/plugins.ts
@@ -1,7 +1,7 @@
-import {resourceProviderPlugin} from './plugins/transact/resource-provider'
+// import {resourceProviderPlugin} from './plugins/transact/resource-provider'
 
 suite('plugins', function () {
     suite('transact', function () {
-        resourceProviderPlugin()
+        // resourceProviderPlugin()
     })
 })
