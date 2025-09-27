a3234dcc3e9430c1b48e4475e64ce72aa1b37020
aaroncox <aaron.cox@greymass.com>
2025-09-27 08:11:17 -0700

    Export `getSessionsMatchingWalletPlugins`

--- src/kit.ts
+++ src/kit.ts
@@ -800,12 +800,7 @@ export class SessionKit {
         try {
             const parsed = JSON.parse(data)
             // Only return sessions that have a wallet plugin that is currently registered.
-            const filtered = parsed.filter((s: SerializedSession) =>
-                this.walletPlugins.some((p) => {
-                    return p.id === s.walletPlugin.id
-                })
-            )
-            return filtered
+            return getSessionsMatchingWalletPlugins(parsed, this.walletPlugins)
         } catch (e) {
             throw new Error(`Failed to parse sessions from storage (${e})`)
         }
@@ -824,4 +819,11 @@ export class SessionKit {
             ui: this.ui,
         }
     }
+}
+
+export function getSessionsMatchingWalletPlugins(
+    sessions: SerializedSession[],
+    walletPlugins: WalletPlugin[]
+) {
+    return sessions.filter((s) => walletPlugins.some((p) => p.id === s.walletPlugin.id))
 }
